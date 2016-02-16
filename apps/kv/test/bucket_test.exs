9651cf7294293fea613542d53dbc2548efb43c61
EliotJones <elioty@hotmail.co.uk>
2016-02-16 21:36:58 +0000

    remove questionable variable name

--- apps/kv/test/bucket_test.exs
+++ apps/kv/test/bucket_test.exs
@@ -14,23 +14,23 @@ defmodule KV.BucketTest do
      end
      
      test "deletes values by key", %{bucket: bucket} do
-        KV.Bucket.put(bucket, "piss", 5)
-        assert KV.Bucket.get(bucket, "piss") == 5
+        KV.Bucket.put(bucket, "rum", 5)
+        assert KV.Bucket.get(bucket, "rum") == 5
         
-        KV.Bucket.delete(bucket, "piss")
-        assert KV.Bucket.get(bucket, "piss") == nil
+        KV.Bucket.delete(bucket, "rum")
+        assert KV.Bucket.get(bucket, "rum") == nil
      end
      
      test "delete returns key value", %{bucket: bucket} do
-        KV.Bucket.put(bucket, "piss", 5)
+        KV.Bucket.put(bucket, "rum", 5)
         
-        assert KV.Bucket.delete(bucket, "piss") == 5
+        assert KV.Bucket.delete(bucket, "rum") == 5
      end
      
      test "put same key twice updates key value", %{bucket: bucket} do
-        KV.Bucket.put(bucket, "piss", 5)
-        KV.Bucket.put(bucket, "piss", 3) 
-        assert KV.Bucket.get(bucket, "piss") == 3
+        KV.Bucket.put(bucket, "rum", 5)
+        KV.Bucket.put(bucket, "rum", 3) 
+        assert KV.Bucket.get(bucket, "rum") == 3
      end
      
      test "add and get works", %{bucket: bucket} do
